e1cf83c0579b522e13ebcfe80a2f6c50d7eb44c8
SaraBasaran <sarabasaran195@gmail.com>
2022-07-23 18:40:39 +0300

    task done

---
+++ src/main/java/EvenOrOddNumber.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenOrOddNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number");
+
+        Scanner sc=new Scanner(System.in);
+
+        int input=sc.nextInt();
+        if (input%2 == 0) {
+            System.out.println(input + "The number is even");
+
+        }else {
+            System.out.println(input + "The number is odd");
+        }
+
+        Scanner input2 = new Scanner(System.in);
+
+        System.out.println("Enter 3 numbers");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/Practises01.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Practises01 {
+
+    public static void main(String[] args) {
+
+               System.out.println("Enter the number");
+
+               Scanner sc=new Scanner(System.in);
+
+        int input=sc.nextInt();
+        if (input>=0){
+            System.out.println(input + "is a positive number");
+
+        }else {
+            System.out.println(input + "is a negative number");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
